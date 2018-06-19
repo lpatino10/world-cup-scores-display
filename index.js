292c10f2f60cdf6b6761545733109eb59f067e5f
Logan Patino <loganpatino10@gmail.com>
2018-06-19 03:51:05 +0000

    feat(display): Add map for country abbreviations

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
 var fetch = require('node-fetch');
-const { execSync } = require('child_process');
+const { execSync, exec } = require('child_process');
+
+const countryMap = {
+  'Argentina': 'ARG',
+  'Australia': 'AUS',
+  'Belgium': 'BEL',
+  'Brazil': 'BRA',
+  'Colombia': 'COL',
+  'Costa Rica': 'CRC',
+  'Croatia': 'CRO',
+  'Denmark': 'DEN',
+  'Egypt': 'EGY',
+  'England': 'ENG',
+  'France': 'FRA',
+  'Germany': 'GER',
+  'Iceland': 'ISL',
+  'Iran': 'IRN',
+  'Japan': 'JPN',
+  'Mexico': 'MEX',
+  'Morocco': 'MAR',
+  'Nigeria': 'NGA',
+  'Panama': 'PAN',
+  'Peru': 'PER',
+  'Poland': 'POL',
+  'Portugal': 'POR',
+  'Russia': 'RUS',
+  'Saudi Arabia': 'KSA',
+  'Senegal': 'SEN',
+  'Serbia': 'SRB',
+  'South Korea': 'KOR',
+  'Spain': 'ESP',
+  'Sweden': 'SWE',
+  'Switzerland': 'SUI',
+  'Tunisia': 'TUN',
+  'Uruguay': 'URU'
+};
 
 // wrap inside polling loop
 /* fetch('https://world-cup-scores.herokuapp.com/')
@@ -16,4 +51,4 @@ const generateTextImage = (text, filename, ledRows) => {
 
 generateTextImage('COL - 3 JPN - 0', 'test.ppm', 16);
 
-execSync(`sudo ../rpi-rgb-led-matrix/examples-api-use/demo --led-no-hardware-pulse --led-rows=16 --led-brightness=50 -D 1 -m 500 test.ppm`)
+exec(`sudo ../rpi-rgb-led-matrix/examples-api-use/demo --led-no-hardware-pulse --led-rows=16 --led-brightness=50 -D 1 -m 500 test.ppm`);
